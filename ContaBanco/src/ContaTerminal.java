abc16625c61e54eabe3b858056acdbf08807e3ec
leandrostclaradev <leandrostclaradev@gmail.com>
2024-09-23 11:38:47 -0400

    Versão Final

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -19,18 +19,18 @@ public class ContaTerminal {
         System.out.printf("Informe sua agência : ");
         String agencia = scanner.nextLine();
 
-        System.out.println("Informe o numero da conta : ");
+        System.out.printf("Informe o numero da conta : ");
         int conta = scanner.nextInt();
 
-        System.out.println("Informe seu saldo : ");
+        System.out.printf("Informe seu saldo : ");
         float saldo = scanner.nextFloat();
 
         System.out.println("\n\n Conta aberta com Sucesso !");
         System.out.println("\n -- INFORMAÇÕES DA CONTA --");
-        System.out.println("\n\nNome: " + nome);
-        System.out.println("\n\nAgência: " + agencia);
-        System.out.println("\n\nConta: " + conta);
-        System.out.println("\n\nSaldo: " + saldo);
+        System.out.println("\nNome: " + nome);
+        System.out.println("Agência: " + agencia);
+        System.out.println("Conta: " + conta);
+        System.out.println("Saldo: " + saldo);
 
     }
 }
